fbc5e3ad8fa79be072060a49ce04f1546c9a9573
Adrian <adrian.botha8@gmail.com>
2018-06-14 16:21:24 +0200

    first commit

---
+++ learning_templates/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Adrian\PycharmProjects\learning_templates\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
